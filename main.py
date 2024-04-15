3251a0ecd679985ae8f195088a7d72547327842e
Nahuel Pierini <nahuelpierini@gmail.com>
2024-04-15 20:46:24 +0200

    add lowercase

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def make_upper_case(word: str):
     # 100s of line of code
     return word.upper()
 
+def make_lower_case(word2: str):
+
+    return word2.lower()
+
 if __name__ == "__main__":
     word = make_upper_case("hola")
+    word2 = make_lower_case("CHAU")
     print(word)
+    print(word2)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import make_upper_case
+from main import make_upper_case, make_lower_case
 
 def test_upper_case():
     test_words = {
@@ -10,3 +10,15 @@ def test_upper_case():
 
     for input_, output_ in test_words.items():
         assert make_upper_case(input_) == output_
+
+def test_lower_case():
+    test_words2 = {
+        "WORD2":"word2",
+        "HOLA":"hola",
+        "CHAU":"chau",
+        "": ""
+
+    }
+
+    for input_, output_ in test_words2.items():
+        assert make_lower_case(input_) == output_
